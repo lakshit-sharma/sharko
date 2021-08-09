e447d849c62f9a4a821d6e0f0d777b98d8e675f8
LAKSHIT SHARMA <slakshit@yahoo.com>
2021-08-09 18:05:19 +0530

    created login/signup web pages, added path to urls, created views.py for it

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('package',views.package, name='package')
+    path('package',views.package, name='package'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name= 'signup')
 ]
--- home/views.py
+++ home/views.py
@@ -19,3 +19,9 @@ def index(request):
 
 def package(request):
     return render(request, 'package.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
